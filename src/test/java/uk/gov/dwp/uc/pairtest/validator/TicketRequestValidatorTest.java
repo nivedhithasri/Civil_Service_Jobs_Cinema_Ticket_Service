ce9eb560f5c5d6282883eec5c0afda15be9dbfb4
nivedhithasri <nive2000.cn@gmail.com>
2025-03-08 18:47:00 +0530

    code enhancement

--- src/test/java/uk/gov/dwp/uc/pairtest/validator/TicketRequestValidatorTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/validator/TicketRequestValidatorTest.java
@@ -2,72 +2,66 @@ package uk.gov.dwp.uc.pairtest.validator;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class TicketRequestValidatorTest {
 
     private TicketRequestValidator validator;
+
+    private static final Long VALID_ACCOUNT_ID = 1L;
+    private static final Long INVALID_ACCOUNT_ID_NULL = null;
+    private static final Long INVALID_ACCOUNT_ID_ZERO = 0L;
+    private static final int MAX_TICKETS_ALLOWED = 25;
+
+    private final Long accountId;
+    private final TicketTypeRequest[] ticketRequests;
+
+    // Fix: Use varargs (TicketTypeRequest...)
+    public TicketRequestValidatorTest(Long accountId, TicketTypeRequest... ticketRequests) {
+        this.accountId = accountId;
+        this.ticketRequests = ticketRequests;
+    }
 
     @Before
     public void setUp() {
         validator = new TicketRequestValidator();
     }
 
-    // Valid case: 2 Adult, 1 Child (should pass)
+    // Valid case: 2 Adults, 1 Child (should pass)
     @Test
     public void shouldPassForValidTicketRequest() {
-        validator.validate(1L,
+        validator.validate(VALID_ACCOUNT_ID,
                 new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2),
                 new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1)
         );
     }
 
-    // Invalid account ID (null or zero)
-    @Test(expected = InvalidPurchaseException.class)
-    public void shouldThrowExceptionForNullAccountId() {
-        validator.validate(null, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
+    // Invalid cases - These should throw InvalidPurchaseException
+    @Parameterized.Parameters(name = "{index}: Invalid Case - Account ID: {0}, Tickets: {1}")
+    public static Collection<Object[]> invalidTicketRequests() {
+        return Arrays.asList(new Object[][]{
+                {INVALID_ACCOUNT_ID_NULL, new TicketTypeRequest[]{new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1)}},
+                {INVALID_ACCOUNT_ID_ZERO, new TicketTypeRequest[]{new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1)}},
+                {VALID_ACCOUNT_ID, new TicketTypeRequest[]{new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2)}}, // No adult ticket
+                {VALID_ACCOUNT_ID, new TicketTypeRequest[]{new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1)}}, // No adult ticket
+                {VALID_ACCOUNT_ID, new TicketTypeRequest[]{}}, // No tickets at all
+                {VALID_ACCOUNT_ID, new TicketTypeRequest[]{new TicketTypeRequest(TicketTypeRequest.Type.ADULT, -2)}}, // Negative ticket count
+                {VALID_ACCOUNT_ID, new TicketTypeRequest[]{
+                        new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 20),
+                        new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 8) // Exceeds limit (26 tickets)
+                }}
+        });
     }
 
     @Test(expected = InvalidPurchaseException.class)
-    public void shouldThrowExceptionForZeroAccountId() {
-        validator.validate(0L, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
-    }
-
-    // No Adult tickets but Child/Infant tickets present
-    @Test(expected = InvalidPurchaseException.class)
-    public void shouldThrowExceptionWhenNoAdultTicketWithChild() {
-        validator.validate(1L,
-                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2)
-        );
-    }
-
-    @Test(expected = InvalidPurchaseException.class)
-    public void shouldThrowExceptionWhenNoAdultTicketWithInfant() {
-        validator.validate(1L,
-                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1)
-        );
-    }
-
-    // Exceeding ticket limit (more than 25)
-    @Test(expected = InvalidPurchaseException.class)
-    public void shouldThrowExceptionForExceedingTicketLimit() {
-        validator.validate(1L,
-                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 20),
-                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 6)
-        );
-    }
-
-    // Zero or negative tickets in request
-    @Test(expected = InvalidPurchaseException.class)
-    public void shouldThrowExceptionForZeroTickets() {
-        validator.validate(1L);
-    }
-
-    @Test(expected = InvalidPurchaseException.class)
-    public void shouldThrowExceptionForNegativeTicketCount() {
-        validator.validate(1L,
-                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, -2)
-        );
+    public void shouldThrowExceptionForInvalidRequests() {
+        validator.validate(accountId, ticketRequests);
     }
 }
